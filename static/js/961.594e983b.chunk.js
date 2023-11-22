1e5844b6701f972e6de56a2c19f8c4ef2a6d601a
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-11-22 17:36:10 +0000

    Deploying to gh-pages from @ PetroBrodetskyi/goit-react-hw-05-movies@e4b0c8d6d76540467ae520e5a0b788e7868664f2 🚀

---
+++ static/js/961.594e983b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[961],{961:function(e,t,a){a.r(t);t.default=function(){}}}]);
+//# sourceMappingURL=961.594e983b.chunk.js.map
